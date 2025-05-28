4c4c02349f7a46c24c9740a5045bb34665015068
wa008 <zphu1024@gmail.com>
2025-05-28 20:24:38 +0800

    tts

--- entry_start.py
+++ entry_start.py
@@ -3,26 +3,45 @@ from download_latest_doc import download_latest_doc
 from gemini_translate import translate_text
 from parse_epub import parse_epub
 import sys 
+import subprocess
+import os
 # python 
 
+checkpoint_path = "/root/.cache/modelscope/hub/models/fishaudio/fish-speech-1.5/firefly-gan-vq-fsq-8x1024-21hz-generator.pth"
+model_path = "/root/.cache/modelscope/hub/models/fishaudio/fish-speech-1.5"
+
+def text_to_audio(text, audio_file):
+    npy_dir = "./temp_npy"
+    text_to_npy(model_path, text, npy_dir)
+    npy_to_audio(checkpoint_path, npy_dir, audio_file)
 
 def main():
     api_key = sys.argv[1]
-    flag, epub_file, text_file = download_latest_doc()
-    # flag, epub_file, text_file = True, "./local_file.epub", "./the_economist/2025-04-26-parse.txt"
+    # flag, epub_file, text_file = download_latest_doc()
+    flag, epub_file, text_file = True, "./local_file.epub", "./the_economist/2025-05-24-parse.txt"
     print (f"flag: {flag}")
     print (f"epub_file: {epub_file}")
     print (f"text_file: {text_file}")
     if flag == False: 
         return 
 
-    parsed_content = parse_epub(epub_file, text_file)
+    # parsed_content = parse_epub(epub_file, text_file)
     
     translated_file = text_file.replace("parse", "translated")
-    translate_text(text_file, translated_file, api_key)
+    # translate_text(text_file, translated_file, api_key)
+
+    datas = open(translated_file, 'r').read().split("\n\n" + "-" * 50 + "\n\n")
+    index = 10000
+    for data in datas:
+        index += 1
+        # title = data.split("\n")[0]
+        # content = "\n".join(data.split("\n")[1: ])
+        audio_file = f"./audio_{index}.wav"
+        text_to_audio(data, audio_file)
 
 
 if __name__ == "__main__":
     for i in range(3):
         main()
+
 
--- tts.py
+++ tts.py
@@ -1,14 +1,8 @@
 import subprocess
 import os
 
-checkpoint_path = "/root/.cache/modelscope/hub/models/fishaudio/fish-speech-1.5/firefly-gan-vq-fsq-8x1024-21hz-generator.pth"
-device = "cpu"
-npy_dir = "./npy"
 
-def text_to_npy():
-    model_path = "/root/.cache/modelscope/hub/models/fishaudio/fish-speech-1.5"
-    text = "4月30日，一项衡量新出口订单的指标跌至2022年以来的最低水平。"
-    
+def text_to_npy(model_path, text, npy_dir):
     script_path = "fish-speech/fish_speech/models/text2semantic/inference.py"
     args = [
         "--text", text,
@@ -18,16 +12,15 @@ def text_to_npy():
     command = ["python", script_path] + args
     subprocess.run(command, check=True)
 
-def npy_to_audio():
-
+def npy_to_audio(checkpoint_path, npy_dir, output_file):
     script_path = "fish-speech/fish_speech/models/vqgan/inference.py"
+    device = "cpu"
     args = [
         "--input-path", npy_dir + "/codes_0.npy", 
         "--checkpoint-path", checkpoint_path,
         "--device", device,
+        "--output-path", output_file
     ]
     command = ["python", script_path] + args
     subprocess.run(command, check=True)
 
-text_to_npy()
-npy_to_audio()
