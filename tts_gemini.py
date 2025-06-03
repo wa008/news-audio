aa697bd487df570ddca19eb7ceedb3a18a7f429f
wa008 <hzp1024@qq.com>
2025-06-03 11:30:01 +0800

    gemini

--- tts_gemini.py
+++ tts_gemini.py
@@ -7,6 +7,7 @@ from google import genai
 from google.genai import types
 import wave 
 import time
+import sys
 from utils import mkdir_path
 
 # Set up the wave file to save the output:
@@ -22,36 +23,13 @@ def gemini_tts(client, user_input, output_file, max_attempts = 1):
     delay = 1
     while attempt_count < max_attempts:
         attempt_count += 1
-        response = client.models.generate_content(
-            model="gemini-2.5-flash-preview-tts",
-            contents="This is a dog",
-            config=types.GenerateContentConfig(
-                response_modalities=["AUDIO"],
-                speech_config=types.SpeechConfig(
-                    voice_config=types.VoiceConfig(
-                        prebuilt_voice_config=types.PrebuiltVoiceConfig(
-                            voice_name='Kore',
-                        )
-                    )
-                ),
-            )
-        )
-        data = response.candidates[0].content.parts[0].inline_data.data
-        wave_file(output_file, data) # Saves the file to current directory
         try:
+            contents = "Read this in chinese: ${user_input}"
+            MODEL_ID = "gemini-2.5-flash-preview-tts"
             response = client.models.generate_content(
-                model="gemini-2.5-flash-preview-tts",
-                contents=user_input,
-                config=types.GenerateContentConfig(
-                    response_modalities=["AUDIO"],
-                    speech_config=types.SpeechConfig(
-                        voice_config=types.VoiceConfig(
-                            prebuilt_voice_config=types.PrebuiltVoiceConfig(
-                                voice_name='Kore',
-                            )
-                        )
-                    ),
-                )
+                model=MODEL_ID,
+                contents = contents,
+                config={"response_modalities": ['Audio']},
             )
             data = response.candidates[0].content.parts[0].inline_data.data
             wave_file(output_file, data) # Saves the file to current directory
@@ -70,3 +48,10 @@ def gemini_process_all_text_to_audio(datas, audio_path, api_key = 'default'):
         output_file = os.path.join(audio_path, f"audio_{index}.wav")
         gemini_tts(client, data, output_file)
         break 
+
+if __name__ == "__main__":
+    # Example
+    datas = ["Say cheerfully: Have a wonderful day!"]
+    audio_path = "tmp_audio_output"
+    api_key = sys.argv[1] if len(sys.argv) > 1 else 'default'
+    gemini_process_all_text_to_audio(datas, audio_path, api_key)
